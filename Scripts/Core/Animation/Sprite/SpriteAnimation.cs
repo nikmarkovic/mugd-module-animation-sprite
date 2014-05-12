7f0e3d8d64b8d741fc5976f3ee5415ba8730c120
nikmarkovic <nik.markovic.91@gmail.com>
2014-05-12 22:23:51 +0200

    first commit

---
+++ Scripts/Core/Animation/Sprite/SpriteAnimation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Animation.Sprite
+{
+    [Serializable]
+    public class SpriteAnimation
+    {
+        [SerializeField]
+        private Texture _texture;
+        [SerializeField]
+        private int _frames;
+        [SerializeField]
+        private int _angles;
+        [SerializeField]
+        private int _fps;
+        private int _currentFrame;
+        private bool _loop;
+        private Func<float> _angle;
+        private Action _onAnimationEnd;
+
+        public bool Loop
+        {
+            get { return _loop; }
+            set { _loop = value; }
+        }
+
+        public int CurrentFrame
+        {
+            get { return _currentFrame; }
+            set { _currentFrame = value; }
+        }
+
+        public Texture Texture
+        {
+            get { return _texture; }
+        }
+
+        public string Name
+        {
+            get { return _texture.name; }
+        }
+
+        public int Frames
+        {
+            get { return _frames; }
+        }
+
+        public int Angles
+        {
+            get { return _angles; }
+        }
+
+        public Vector2 Scale
+        {
+            get { return new Vector2(1f/_frames, 1f/_angles); }
+        }
+
+        public int Fps
+        {
+            get { return _fps; }
+        }
+
+        public float Spf
+        {
+            get { return 1f/_fps; }
+        }
+
+        public void SetAngleFunction(Func<float> angle)
+        {
+            _angle = angle;
+        }
+
+        public void SetOnAnimationEnd(Action onAnimationEnd)
+        {
+            _onAnimationEnd = onAnimationEnd;
+        }
+
+        public Vector2 GetNextFrame()
+        {
+            _currentFrame = (_currentFrame + 1)%_frames;
+            var x = (float) _currentFrame/_frames;
+            var y = 1 - 1f/_angles - GetAngle()/360;
+            return new Vector2(x, y);
+        }
+
+        public void OnAnimationEnd()
+        {
+            if (_onAnimationEnd == null) return;
+            _onAnimationEnd();
+        }
+
+        private float GetAngle()
+        {
+            if (_angle == null) return 0;
+            var angle = (Mathf.Round((int) _angle()/GetStep())*GetStep())%360;
+            return angle < 0 ? angle + 360 : angle;
+        }
+
+        private float GetStep()
+        {
+            return 360f/_angles;
+        }
+    }
+}
---
+++ Scripts/Core/Animation/Sprite/SpriteAnimator.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Animation.Sprite
+{
+    [AddComponentMenu("Mugd/Animation/Sprite/Animator")]
+    [RequireComponent(typeof (MeshRenderer))]
+    public class SpriteAnimator : MonoBehaviour
+    {
+        [SerializeField] 
+        private SpriteAnimation[] _playlist;
+        private readonly LinkedList<SpriteAnimation> _queue = new LinkedList<SpriteAnimation>();
+        private float _lastFrameTime;
+
+        public SpriteAnimation Current { get; private set; }
+
+        private void Awake()
+        {
+            renderer.sharedMaterial = new Material(renderer.sharedMaterial);
+        }
+
+        private void Update()
+        {
+            if (Current == null && _queue.Count > 0)
+            {
+                Play(_queue.First());
+                _queue.RemoveFirst();
+            }
+            if (Current == null || !(Time.time - _lastFrameTime >= Current.Spf)) return;
+            renderer.sharedMaterial.SetTextureOffset("_MainTex", Current.GetNextFrame());
+            _lastFrameTime = Time.time;
+            if (Current.CurrentFrame != Current.Frames - 1) return;
+            Current.OnAnimationEnd();
+            if (!Current.Loop) Stop();
+        }
+
+        public SpriteAnimation PlayForce(string animationName, bool loop)
+        {
+            var newAnimation = FindAnimation(animationName);
+            if (newAnimation == null) return null;
+            newAnimation.CurrentFrame = -1;
+            newAnimation.Loop = loop;
+            _queue.AddFirst(newAnimation);
+            Stop();
+            return newAnimation;
+        }
+
+        public SpriteAnimation Play(string animationName, bool loop)
+        {
+            var newAnimation = FindAnimation(animationName);
+            if (newAnimation == null) return null;
+            newAnimation.CurrentFrame = -1;
+            newAnimation.Loop = loop;
+            _queue.AddLast(newAnimation);
+            return newAnimation;
+        }
+
+        public void StopForce()
+        {
+            _queue.Clear();
+            Current = null;
+        }
+
+        public void Stop()
+        {
+            Current = null;
+        }
+
+        private void Play(SpriteAnimation newAnimation)
+        {
+            Current = newAnimation;
+            renderer.sharedMaterial.SetTexture("_MainTex", Current.Texture);
+            renderer.sharedMaterial.SetTextureScale("_MainTex", Current.Scale);
+            _lastFrameTime = Time.time;
+        }
+
+        private SpriteAnimation FindAnimation(string animationName)
+        {
+            var animations = _playlist.Where(a => a.Name.ToUpper().Contains(animationName.ToUpper())).ToList();
+            Random.seed = (int) (Time.time*1000);
+            return animations.Any() ? animations[Random.Range(0, animations.Count)] : null;
+        }
+    }
+}
